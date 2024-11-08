f793c54c1164ddd6442025314fd382e8333f6ead
Nytaneliah Ranaivoson <nytanelih@gmail.com>
2024-11-08 12:40:00 +0100

    router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
+
+const routes = [
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-  ],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
